edf62139213f8a8189c508026d26cf4fbfd147a5
Namul Alam Tuhin <natuhin02@gmail.com>
2026-07-21 05:43:18 +0600

    package.json  update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <Routes>
             <Route
                 path="/"
-                element={<Navigate to="/login" replace />}
+                element={<Navigate to="/register" replace />}
             />
 
             <Route
